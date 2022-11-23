e1001b7ae55b48c17abaa960b289913695e3bb81
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-23 14:58:27 +0330

    add: salt to private key

--- key.py
+++ key.py
@@ -1,10 +1,18 @@
 # importing os module to generate salt
 import os
+import base64
 
 
 
-# constant variables
+# key module constant variables.
+
+# file path of private key.
 PRIVATE = "private.key"
+
+# taking maximum advantage of the full strength of AES encryption requires
+# a password of approximately 32 characters for 128-bit encryption and 
+# 64 characters for 256-bit encryption.
+KEY_LIMIT = 64
 
 
 
@@ -18,8 +26,25 @@ def read_key():
         with open(PRIVATE, "r") as file:
             private_key = file.readline()
 
-            return private_key
+            return __add_salt(private_key)
     except EnvironmentError:
         print("[ERROR][PRIVATE KEY] private key not found")
         exit()
+ 
+
+"""
+__add_salt(key: any key to add salt to it)
+
+Adds a random string into the end of our key.
+"""
+def __add_salt(key):
+    size = KEY_LIMIT - len(key)
     
+    if size <= 0:
+        return key
+
+    salt = base64.b64encode(os.urandom(size)).decode("utf-8")[:size]
+
+    print(f'salt:\n\tvalue:{salt}\n\tsize:{len(salt)}\n')
+
+    return key + salt
---
+++ main.py
@@ -0,0 +1,7 @@
+# importing key module to read the private key
+from key import read_key
+
+
+
+if __name__ == "__main__":
+    print(read_key())
