65092a0e4aa6e9a46ad75577062fdf1b01562f29
amirhnajafiz <najafizadeh21@gmail.com>
2022-11-23 16:03:15 +0330

    update: key salt generation

--- key.py
+++ key.py
@@ -1,6 +1,6 @@
 # importing os and pbkdf2 modules to generate key with salt
 import os, binascii
-from backports.pbkdf2 import pbkdf2_hmac
+from pbkdf2 import PBKDF2
 
 
 
@@ -12,7 +12,7 @@ PRIVATE = "private.key"
 # taking maximum advantage of the full strength of AES encryption requires
 # a password of approximately 32 characters for 128-bit encryption and 
 # 64 characters for 256-bit encryption.
-KEY_LIMIT = 64
+KEY_LIMIT = 32
 
 
 
@@ -45,12 +45,10 @@ def __add_salt(key):
     # salt – securely-generated random bytes, 
     # e.g. "df1f2d3f4d77ac66e9c5a6c3d8f921b6" (minimum 64 bits, 128 bits is recommended)
     # generating 256 bit salt
-    salt = os.urandom(32)
+    salt = os.urandom(KEY_LIMIT)
 
-    # sha256 hash-function for calculating HMAC, e.g. SHA256
-    # 50000 iterations-count, e.g. 1024 iterations
-    # 32 derived-key-len is for the output, e.g. 32 bytes (256 bits)
-    key = pbkdf2_hmac("sha256", key, salt, 50000, 32)
+    # generating a 256 bit key with salt
+    key = PBKDF2(key, salt).read(KEY_LIMIT) # 256-bit key
 
     # hexlifying the generated key
     key = binascii.hexlify(key)
